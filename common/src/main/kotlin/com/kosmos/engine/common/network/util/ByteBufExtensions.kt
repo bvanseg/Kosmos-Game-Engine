8056e6738e82a62c5903f39fa63ae8e5d1a75355
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2020-12-25 12:49:06 -0500

    Added ByteBuf extensions for reading/writing Strings and Versions.

--- common/src/main/kotlin/com/kosmos/engine/common/network/util/ByteBufExtensions.kt
+++ common/src/main/kotlin/com/kosmos/engine/common/network/util/ByteBufExtensions.kt
@@ -1,7 +1,19 @@
 package com.kosmos.engine.common.network.util
 
+import bvanseg.kotlincommons.project.Version
 import io.netty.buffer.ByteBuf
+import java.nio.charset.StandardCharsets
 import java.util.*
+
+fun ByteBuf.writeUTF8String(string: String) {
+    this.writeShort(string.length)
+    this.writeBytes(string.toByteArray())
+}
+
+fun ByteBuf.readUTF8String(): String {
+    val length = this.readShort().toInt()
+    return this.readBytes(length).toString(StandardCharsets.UTF_8)
+}
 
 fun ByteBuf.writeUUID(uuid: UUID) {
     this.writeLong(uuid.mostSignificantBits)
@@ -9,3 +21,12 @@ fun ByteBuf.writeUUID(uuid: UUID) {
 }
 
 fun ByteBuf.readUUID(): UUID = UUID(this.readLong(), this.readLong())
+
+fun ByteBuf.writeVersion(version: Version) {
+    this.writeInt(version.major)
+    this.writeInt(version.minor)
+    this.writeInt(version.patch)
+    this.writeUTF8String(version.label)
+}
+
+fun ByteBuf.readVersion(): Version = Version(this.readInt(), this.readInt(), this.readInt(), this.readUTF8String())
